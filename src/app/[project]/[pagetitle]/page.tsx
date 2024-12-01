5c7a5eaeef03fed8389690f061a77541bb9ca415
meganii <meganii@outlook.jp>
2024-12-01 15:22:34 +0900

    Update

--- src/app/[project]/[pagetitle]/page.tsx
+++ src/app/[project]/[pagetitle]/page.tsx
@@ -1,6 +1,7 @@
 import { parse } from '@progfay/scrapbox-parser'
 import { Page } from '../../../components/Page'
 
+const reIcon = /\[.+?\.icon\]/;
 
 type Props = {
     params: Promise<{
@@ -9,18 +10,18 @@ type Props = {
     }>
 }
 
-type Line = {
+type SbLine = {
     text: string
 }
 
 type SbPage = {
-    lines: Line[]
+    lines: SbLine[]
 }
 
 
 const toJson = (response : string) => {
     const json : SbPage = JSON.parse(response)
-    return json.lines.map(l => l.text)
+    return json.lines
 } 
 
 export default async function PageTitle({params} : Props) {
@@ -28,25 +29,33 @@ export default async function PageTitle({params} : Props) {
     const url = `https://scrapbox.io/api/pages/${project}/${pagetitle}/`
     const response = await fetch(url)
     const text: string = await response.text()
-    const lines = toJson(text)
+    const sblines = toJson(text)
 
-    const diaries : string[] = []
-    let diary = ''
-    for (const lineText of lines) {
-        diary += lineText + '\n'
-        if (lineText.trim() === '') {
-            diaries.push(diary)
-            diary = ''
+    const diaries : SbLine[][] = []
+    let chank : SbLine[] = []
+    for (const sbline of sblines) {
+        if ('' !== sbline.text.trim()) {
+            chank.push(sbline)
+        } else if (chank.length > 0) {
+            diaries.push(chank)
+            chank = []
+        } else {
             continue
         }
     }
+    if (chank.length > 0) {
+        diaries.push(chank)
+        chank = []
+    }
 
-    const list = diaries.map((t, index) => {
+    const list = diaries.map((sblines, index) => {
         return (
             <>
-                <li key={index} className='bg-neutral-100 rounded-3xl p-10 m-10'>
-                    <div className='max-h-60 overflow-hidden'>
-                        <Page blocks={parse('\n' + t)} />
+                <li key={index} className='bg-neutral-100 rounded p-1 m-2'>
+                    <div className=''>
+                        { sblines.map((sbline, i) => (
+                            <Page key={`${index}-${i}`} blocks={parse("\n" + sbline.text.replace(/^[ \t　]+?/, ''))} ></Page>
+                        ))}
                     </div>
                 </li>
             </>
